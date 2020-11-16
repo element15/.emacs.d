8f39ed3bdb40a7bda793defe0eba4e7158278833
Christian Moomaw <element15@phosfor.us>
2020-11-15 19:44:34 -0600

    Set custom modifier keys for macOS

--- init.el
+++ init.el
@@ -92,3 +92,7 @@
 ;;   (princ (with-current-buffer buffer
 ;;     (format "<!DOCTYPE html><html><title>Impatient Markdown</title><xmp theme=\"united\" style=\"display:none;\"> %s  </xmp><script src=\"http://strapdownjs.com/v/0.2/strapdown.js\"></script></html>" (buffer-substring-no-properties (point-min) (point-max))))
 ;;   (current-buffer)))
+
+;; macOS-specific keybind options
+(setq-default mac-command-modifier 'control)
+(setq-default mac-control-modifier 'meta)
